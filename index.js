bacfa796979651c3cd5bf14a815a2616e3b4a43b
Stornelli23 <santystornelli@hotmail.com>
2024-02-13 16:40:30 +0100

    Added some config. preparing to launch the app

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
-// app.use(morgan('tiny'))
 morgan.token('req-body', (req, res) => {
   return JSON.stringify(req.body);
 });
@@ -102,7 +103,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
